c697d76df5c12eaced6735401dbe58c4119df894
belukasz <lbombol@gmail.com>
2024-10-30 01:26:16 +0100

    Initial commit

---
+++ app/scrapper.go
@@ -0,0 +1,88 @@
+package scrapper
+
+import (
+	"context"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/chromedp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Connection struct {
+	DepartureTime string
+	ArrivalTime   string
+	Price         float64
+	Name          string
+	Duration      string
+}
+
+func ScrapeOneDay(start string, end string, date string) ([]Connection, error) {
+	url := fmt.Sprintf("https://koleo.pl/rozklad-pkp/%s/%s/%s/all/EIP--EIP-IC", start, end, date)
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	ctx, cancelTimeout := context.WithTimeout(ctx, 15*time.Second)
+	defer cancelTimeout()
+
+	var renderedContent string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Sleep(3*time.Second),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			rootNode, err := dom.GetDocument().Do(ctx)
+			if err != nil {
+				return err
+			}
+			renderedContent, err = dom.GetOuterHTML().WithNodeID(rootNode.NodeID).Do(ctx)
+			return err
+		}),
+	)
+	document, err := goquery.NewDocumentFromReader(strings.NewReader(renderedContent))
+	if err != nil {
+		return nil, err
+	}
+
+	var connections []Connection
+	document.Find(".has-train-nr").Each(func(i int, s *goquery.Selection) {
+		var (
+			from     string
+			to       string
+			train    string
+			brand    string
+			price    float64
+			duration string
+		)
+		fromQuery := ".medium-3.small-2.columns.time.from"
+		timeToQuery := ".large-3.large-offset-1.medium-3.medium-offset-0.small-2.columns.time.to"
+		trainNumberQuery := ".train-number-details"
+
+		from = strings.TrimSpace(s.Find(fromQuery).First().Text())
+		to = strings.TrimSpace(s.Find(timeToQuery).First().Text())
+
+		train = strings.TrimSpace(s.Find(trainNumberQuery).First().Text())
+		brand = strings.TrimSpace(s.Find(".brand-logo").First().Text())
+		duration = strings.ReplaceAll(strings.TrimSpace(s.Find(".travel-time-value").First().Text()), "h", ":")
+		priceParts := strings.TrimSpace(s.Find(".price-parts").First().Text())
+		priceParts = strings.ReplaceAll(priceParts, "\u00a0", "")
+		priceParts = strings.ReplaceAll(priceParts, "zł", "")
+		priceParts = strings.ReplaceAll(priceParts, ",", ".")
+
+		priceParsed, err := strconv.ParseFloat(priceParts, 64)
+		if err != nil {
+			panic(err)
+		}
+		price = priceParsed
+		conn := Connection{
+			DepartureTime: from,
+			ArrivalTime:   to,
+			Price:         price,
+			Name:          brand + "-" + train,
+			Duration:      duration,
+		}
+		connections = append(connections, conn)
+	})
+	return connections, nil
+}
---
+++ go.mod
@@ -0,0 +1,30 @@
+module belukasz/pkp
+
+go 1.23
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20241022234722-4d5d5faf59fb
+	github.com/chromedp/chromedp v0.11.1
+	github.com/spf13/cobra v1.8.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+	github.com/jedib0t/go-pretty/v6 v6.6.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.2
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
