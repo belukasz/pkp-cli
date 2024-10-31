61b85d77397910481b915db0200d236e88d27151
belukasz <lbombol@gmail.com>
2024-10-31 01:18:06 +0100

    a bit better

--- main.go
+++ main.go
@@ -5,11 +5,8 @@ package main
 
 import (
 	"github.com/belukasz/pkp-cli/cmd"
-	// "github.com/belukasz/pkp-cli/app"
 ) 
 
 func main() {
 	cmd.Execute()
-	// scrapped := scrapper.ScrapeConnections(30, "06:00", "EIP", "krakow", "warszawa")
-	// scrapper.PrintTable(scrapped, true, 3)
 }
